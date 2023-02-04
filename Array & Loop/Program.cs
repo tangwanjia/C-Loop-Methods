9521cd0f9145fd1be7b59e7855e53a97dae2f2c0
Claire Wanjia Tang <113147894+tangwanjia@users.noreply.github.com>
2023-02-04 11:56:53 -0500

    Add files via upload

---
+++ Array & Loop/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using lab3_a;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Lab3 implemented by Wanjia Tang \n");
+
+        Console.WriteLine("The initial unsorted integer array is:");
+        int[] numbers = helper.GenerateIntArrayWithRandValue(100);
+        //call the function of GIA
+        helper.PrintIntArrayToConsole(numbers);
+
+
+        int[] index = helper.GenerateIntArrayWithRandValue(100);
+        numbers.CopyTo(index, 0); //copy from the first array,first parameter is the orignial array, second is start from..
+
+        int swaps = helper.bubbleSort(index);
+        Console.WriteLine("Buble Sort made " + swaps + " swaps to sort the data");
+        helper.PrintIntArrayToConsole(index);
+
+        string answer = "Y";
+        while (answer is "Y" ) //back to step e and do the loop
+        {
+            Console.Write("enter an integer to search:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int numOfComparison;      
+            int lsResult = helper.LinearSearch(numbers, num, out numOfComparison);
+            //call linear search
+
+            if (lsResult != 0)
+            {
+               Console.WriteLine("Linear search made " + numOfComparison + " comparisons to find out that" + num + " is at index " + (num - 1) + " in this unsorted array \n");
+            };
+            
+            int bsResult = helper.BinarySearch( index , swaps, out numOfComparison);
+            if (bsResult != 0)
+            {
+                Console.WriteLine("Binary search made " + numOfComparison + " comparisons to find out that" + num + " is at index" + (num - 1) + " in this unsorted array \n");           
+            };
+            Console.WriteLine("Would you like to search another integer?(Y/N)");
+            answer = Console.ReadLine();
+        } 
+    }
+}
+ 
---
+++ Array & Loop/helper.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3_a
+{
+    internal class helper
+    {
+        public static int[] GenerateIntArrayWithRandValue(int sizeOfArray) //random numbers generated
+        {
+            int[] array = new int[sizeOfArray];
+            Random rdn = new Random();
+
+            for (int j = 0; j < sizeOfArray; ++j)
+            {
+                array[j] = rdn.Next(0,100);
+
+            }
+            return array;
+        }
+        public static void PrintIntArrayToConsole(int[] arrayToPrint)
+        {
+            int count = 0;
+            for (int i = 0; i < arrayToPrint.Length; i++)
+            {
+                Console.Write(arrayToPrint[i] + " " + "\t");
+                count++;
+                if (count == 10)
+                {
+                    Console.WriteLine("\t");
+                    count = 0;
+                }
+            }
+
+        }
+
+        public static int LinearSearch(int[] haystack, int niddle, out int numOfComparison)
+        {
+           numOfComparison = 0;
+            for (int i = 0; i < haystack.Length; i++)
+            {
+                numOfComparison++;
+                if (niddle == haystack[i])
+                {
+                    return i;
+                }   
+            }
+            return -1;
+        }
+
+        public static int bubbleSort (int[] arr)
+        {
+              int temp;
+              int numLength = arr.Length;
+              int swaps = 0;
+            //sorting an array  
+              for (int i = 0; (i < (numLength - 1)); i++)
+              { 
+               
+                for (int j = 0; j < (numLength - j - 1); j++)
+                {
+                    if (arr[j + 1] < arr[j])
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                        swaps++;
+                    }
+                }              
+              }
+              return swaps;
+
+            //Console.WriteLine("Bubble sort made" + " " + "swaps to sort the data" );
+            //    //Sorted array  
+            //  foreach (int num in arr)
+            //  {
+            //    Console.Write("\t {0}", num);
+            //  }
+            //  Console.Read();
+            //    return arr;
+        }
+        public static int BinarySearch(int[] haystack, int niddle, out int numOfComparison)
+        {
+            int minNum = 0;
+            int maxNum = haystack.Length - 1;
+            numOfComparison= 0;
+
+            while (minNum <= maxNum)
+            {
+                int mid = (minNum+maxNum)/2;
+                numOfComparison++;
+                if (niddle == haystack[mid])
+                {
+                    return mid;
+                }
+                else if (niddle < haystack[mid])
+                {
+                    maxNum = mid - 1;
+                }
+                else
+                { 
+                    minNum = mid + 1;      
+                }
+            }
+            return -1;
+        }
+    }
+}
